a89c47523b077928413274a40290c73085439bfe
Aga Koltun <agnieszka.koltun.89@gmail.com>
2022-01-19 15:34:25 +0000

    Rooms and Exits added

--- src/main/java/others/Room.java
+++ src/main/java/others/Room.java
@@ -6,6 +6,24 @@ public class Room {
 
    private ArrayList<Exit> exits;
 
+   public Room() {
+      this.exits = new ArrayList<>();
+   }
 
+   public void generateExits() {
+      Exit[] exits2 = {Exit.EAST, Exit.NORTH, Exit.SOUTH, Exit.WEST};
 
+      int counter = (int)Math.floor(Math.random()*(4-1+1)+1);
+      int randomI = (int)Math.floor(Math.random()*(3-1+1)+1);
+
+      while (counter > 0) {
+         exits.add(exits2[randomI]);
+         randomI -= 1;
+      }
+   }
+
+   public void exitChosen() {
+      Room nextRoom = new Room();
+      generateExits();
+   }
 }
