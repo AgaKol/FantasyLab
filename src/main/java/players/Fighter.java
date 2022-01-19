03ae08997d7f681fce17e20654bb51a1dbc14a38
Aga Koltun <agnieszka.koltun.89@gmail.com>
2022-01-19 13:37:50 +0000

    Changes by Roger

--- src/main/java/players/Fighter.java
+++ src/main/java/players/Fighter.java
@@ -1,4 +1,30 @@
 package players;
 
-public class Fighter {
+import behaviours.IWeapon;
+import enemies.Enemy;
+
+public abstract class Fighter extends Player{
+    IWeapon weapon;
+
+    public Fighter(String _name, int _HP, IWeapon _weapon){
+        super(_name, _HP);
+        this.weapon = _weapon;
+    }
+
+    public IWeapon getWeapon() {
+        return weapon;
+    }
+
+    public void setWeapon(IWeapon weapon) {
+        this.weapon = weapon;
+    }
+
+    public void fighterAttack(Enemy _enemy){
+        weapon.attack(_enemy);
+    }
+
+    public void doubleAttack(Enemy enemy){
+        fighterAttack(enemy);
+        fighterAttack(enemy);
+    }
 }
